bb506cec16316747c4a5096b9cfe3c178f1d3b3b
Alex Chaplianka <alexc@zeekit.co>
2018-06-05 10:18:56 +0300

    Initial commit

---
+++ src/main/java/tk/aclowkey/App.java
@@ -0,0 +1,18 @@
+package tk.aclowkey;
+
+import org.jooby.Jooby;
+
+/**
+ * @author jooby generator
+ */
+public class App extends Jooby {
+
+  {
+    get("/", () -> "Hello World!");
+  }
+
+  public static void main(final String[] args) {
+    run(App::new, args);
+  }
+
+}
